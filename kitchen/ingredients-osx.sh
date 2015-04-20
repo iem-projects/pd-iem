8c726d92c5c16d306610061551fd726b159d04a4
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-04-20 23:37:48 +0200

    dependency installer scripts

---
+++ kitchen/ingredients-osx.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+## setup some environmental variables
+SCRIPTDIR=${0%/*}
+PDVERSION=0.46-6
+
+
+## some helper functions
+error() {
+ echo "$@" 1>&2
+}
+ensuredir() {
+   mkdir -p "$1" 2>/dev/null && cd "$1" && pwd
+}
+
+
+## the actual work
+# 0. setup all paths
+BUILDDIR=$(ensuredir "${SCRIPTDIR}/../build")
+if [ ! -d "${BUILDDIR}" ]; then error "no BUILDDIR"; exit 1; fi
+
+# 1. install dependencies
+cd "${BUILDDIR}"
+# 1.1 update brew
+brew update
+brew install pkg-config gettext
+brew link gettext --force
+brew install coreutils
+
+# 1.2 install Pd
+wget http://msp.ucsd.edu/Software/pd-${PDVERSION}-64bit.mac.tar.gz
+tar -xf pd-${PDVERSION}-64bit.mac.tar.gz
+ln -s "${BUILDDIR}/Pd-${PDVERSION}-64bit.app/Contents/Resources/" pd
+
+# 1.X fix permissions
+chmod -R u+rwX .
+
---
+++ kitchen/ingredients.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+## setup some environmental variables
+SCRIPTDIR=${0%/*}
+
+## some helper functions
+error() {
+ echo "$@" 1>&2
+}
+ensuredir() {
+   mkdir -p "$1" 2>/dev/null && cd "$1" && pwd
+}
+
+
+## the actual work
+# 0. setup all paths
+SCRIPTDIR=$(ensuredir "${SCRIPTDIR}")
+BUILDDIR=$(ensuredir "${SCRIPTDIR}/../build")
+BASEDIR=$(ensuredir "${SCRIPTDIR}/..")
+DESTDIR=$(ensuredir "${BASEDIR}/dist/pd-iem")
+if [ ! -d "${SCRIPTDIR}" ]; then error "no SCRIPTDIR"; exit 1; fi
+if [ ! -d "${BUILDDIR}" ]; then error "no BUILDDIR"; exit 1; fi
+if [ ! -d "${BASEDIR}" ]; then error "no BASEDIR"; exit 1; fi
+if [ ! -d "${DESTDIR}" ]; then error "no DESTDIR"; exit 1; fi
+
+
+INSTALLSCRIPT="${0%.sh}-${TRAVIS_OS_NAME}.sh
+
+if [ -x "${INSTALLSCRIPT}" ]; then
+ "./${INSTALLSCRIPT}"
+else
+ error "no install-script found for: ${TRAVIS_OS_NAME}"
+ error "assuming all is well"
+fi
