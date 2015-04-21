f0419339fd4e8be9d1a8e14fd744fcf463f5b36a
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-04-21 22:06:39 +0200

    chef d'pasty for w32

--- kitchen/pasty-w32.sh
+++ kitchen/pasty-w32.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-echo "no recursive library embedding for W32 yet" 1>&2
+if [ $# -ne 1 ]; then
+	error "Usage: $0 external/dir/"
+	error "  i.e. $0 iemmatrix/"
+	exit
+fi
+
+MAXITERATIONS=10
+DIR=$1
+
+find "${DIR}" -type f -name "*.dll" -print0 | xargs -0 ${0%/*}/pastyroll-w32.sh libs
+RES=$?
+
+count=0
+while test 0 -lt ${RES}; do
+    error "########################## iteration ${count} ##################"
+    find "${DIR}" -type f -print0 | xargs -0 ${0%/*}/pastyroll-w32.sh ''
+    RES=$?
+    if [ ${count} -gt ${MAXITERATIONS} ]; then
+      RES=0
+    fi
+    count=$((count+1))
+done
---
+++ kitchen/pastyroll-w32.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# This script takes a binary file and an optional relative path (with resp. to the file),
+# and puts all non-standard dependencies of the binary into that relative path
+# and makes the binary use these.
+# originally written by  <hans@at.or.at>
+# hacked upon by <zmoelnig@iem.at>
+#
+
+error() {
+    echo "$@" 1>&2
+}
+if [ $# -lt 2 ]; then
+    error "Usage: $0 <embedpath> <binary> ..."
+    error "   <embedpath> is ignored (things end up besides <binary>)"
+    error "  i.e. $0 libs tmp/foo.pd_darwin tmp/bar.d_fat"
+    error "         puts all dependencies of foo.pd_darwin bar.d_fat into tmp/libs"
+    error "  i.e. $0 "" tmp/libs/libgoo.dylib"
+    error "         puts all dependencies into tmp/libs"
+    exit 0
+fi
+
+RELPATH=$1
+shift
+
+count=0
+while [ 0 -lt $# ]; do
+    bin=$1
+    shift
+    dir=${bin%/*}/${RELPATH}
+    for lib in $(i686-w64-mingw32-objdump -x ${bin} | sed -n -e '/DLL Name:/p' | cut -d' ' -f3); do
+	if [  -e "${dir}/${lib}" ]; then
+	    error "skipping local copy of already existing library ${lib}"
+	else
+	    ## search for the library
+	    find . -name "${lib}" -exec cp \{\} "${dir}" \;
+	    if [  -e "${dir}/${lib}" ]; then
+		error "installed '${lib}' into '${dir}'"
+		count=$((count+1))
+	    else
+		error "skipping unfound dependency ${lib}"
+	    fi
+	fi
+    done
+done
+
+exit ${count}
