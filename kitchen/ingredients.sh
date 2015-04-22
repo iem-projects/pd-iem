4e881c9b4745e08df15b80edfb2c1aaf62d0645e
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-04-22 12:50:22 +0200

    don't manually mangle OS-name: use mangle_os instead

--- kitchen/ingredients.sh
+++ kitchen/ingredients.sh
@@ -24,10 +24,6 @@ if [ ! -d "${BASEDIR}" ]; then error "no BASEDIR"; exit 1; fi
 if [ ! -d "${DESTDIR}" ]; then error "no DESTDIR"; exit 1; fi
 
 
-if [ "x${TRAVIS_OS_NAME}" = "xlinux" ]; then
- TRAVIS_OS_NAME=w32
-fi
-
 INSTALLSCRIPT="${0%.sh}-$(${SCRIPTDIR}/mangle_os.sh ${TRAVIS_OS_NAME}).sh"
 
 if [ -x "${INSTALLSCRIPT}" ]; then
