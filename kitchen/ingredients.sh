64c1a12fe3fcd1802e886bf36cbd218b7a11aa43
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-05-04 14:53:27 +0200

    create config.mk with some compile-time detection

--- kitchen/ingredients.sh
+++ kitchen/ingredients.sh
@@ -24,12 +24,21 @@ if [ ! -d "${BUILDDIR}" ]; then error "no BUILDDIR"; exit 1; fi
 if [ ! -d "${BASEDIR}" ]; then error "no BASEDIR"; exit 1; fi
 if [ ! -d "${DESTDIR}" ]; then error "no DESTDIR"; exit 1; fi
 
+# 1. clear conf-file
+CONFFILE="${BUILDDIR}/config.mk"
+echo "# automatically generated config-file (makefile snippet)" > "${CONFFILE}"
 
 INSTALLSCRIPT="${0%.sh}-${OS}.sh"
 
 if [ -x "${INSTALLSCRIPT}" ]; then
  "${INSTALLSCRIPT}"
 else
- error "no install-script found ${INSTALLSCRIPT}"
+ error "no OS-specific install-script found ${INSTALLSCRIPT}"
  error "assuming all is well"
 fi
+
+PTHREAD_CHECK=${BASEDIR}/closet/check-pthread.sh
+test -x "${PTHREAD_CHECK}" && "${PTHREAD_CHECK}" && echo "HAVE_PTHREADS=1" >> "${CONFFILE}"
+
+
+exit 0
