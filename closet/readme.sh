de327e72a7e8246a1a497aba8bca43efa7716362
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-05-06 17:27:32 +0200

    get git rev

--- closet/readme.sh
+++ closet/readme.sh
@@ -2,6 +2,10 @@
 
 . ${0%/*}/common
 
+error() {
+    echo "$@" 1>&2
+}
+
 getlibraries() {
     for lib in "${SCRIPTDIR}/../recipes"/*."${OS}"; do
         lib=${lib##*/}
@@ -16,8 +20,19 @@ getpdversion() {
         | sort | tail -1
 }
 
+getrevision() {
+    local REV
+    local SHORTREV=$(git rev-parse --short HEAD 2>/dev/null)
+    REV=$(git describe --long --tags 2>/dev/null | sed -e 's|-[^-]*$||')
+    if [  -x "${REV}" ]; then
+	REV="$(git rev-parse --short HEAD 2>/dev/null)-$(git rev-list --count HEAD 2>/dev/null)"
+    fi
+    test -z "$(git status --untracked-files=normal --porcelain 2>/dev/null)" || REV="${REV}*"
+    echo "${REV}"
+}
 
-PDIEMVERSION=?
+
+PDIEMVERSION=$(getrevision)
 PDVERSION=$(getpdversion)
 LIBRARIES=$(getlibraries)
 TIMESTAMP=$(LANG=C date)
