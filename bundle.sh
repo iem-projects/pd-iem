8e0a6e1fcbb3effaa90baadd76ba23db769d8ef1
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-05-07 10:42:00 +0200

    generate README.txt

--- bundle.sh
+++ bundle.sh
@@ -2,4 +2,5 @@
 
 . ${0%/*}/closet/common
 
+${SCRIPTDIR}/../closet/readme.sh $@ > "${DESTDIR}/pd-iem/README.txt"
 make -C "${BUILDDIR}" -f "${SCRIPTDIR}/kitchen/cook" DESTDIR="${DESTDIR}/pd-iem" TARGETOS="${OS}" PDDIR="${PDDIR}" PDINCLUDE="${PDDIR}/src" install
