e06a2a6a579257b43760b32bd388028171882013
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-05-29 22:53:48 +0200

    enfore distdir for README
    make sure we do have DESTDIR/pd-iem before trying to put a README.txt there

--- bundle.sh
+++ bundle.sh
@@ -3,5 +3,6 @@ ARGS=$@
 
 . ${0%/*}/closet/common
 
+mkdir -p "${DESTDIR}/pd-iem/"
 ${SCRIPTDIR}/closet/readme.sh ${ARGS} > "${DESTDIR}/pd-iem/README.txt"
 make -C "${BUILDDIR}" -f "${SCRIPTDIR}/kitchen/cook" DESTDIR="${DESTDIR}/pd-iem" TARGETOS="${OS}" PDDIR="${PDDIR}" PDINCLUDE="${PDDIR}/src" install
